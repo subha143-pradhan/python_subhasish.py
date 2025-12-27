d91d262f81c05e738b4c238cedb6a7e32716cd05
subha143-pradhan <pradhansubhasish169@gmail.com>
2025-12-27 18:13:04 +0530

    Add files via upload

---
+++ 02_problem2.py
@@ -0,0 +1,17 @@
+marks = []
+
+f1 = input("Enter marks of 1st student:")
+marks.append(f1)
+f2 = input("Enter marks of 2nd student:")
+marks.append(f2)
+f3 = input("Enter marks of 3rd student:")
+marks.append(f3)
+f4 = input("Enter marks of 4th student:")
+marks.append(f4)
+f5 = input("Enter marks of 5th student:")
+marks.append(f5)
+f6 = input("Enter marks of 6th student:")
+marks.append(f6)
+
+marks.sort()
+print(marks)
